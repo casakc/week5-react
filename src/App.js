09430725846f8e3f7e2fee196ab9894016f56fda
casakc <138028480+casakc@users.noreply.github.com>
2024-10-01 18:13:28 +0200

    Added GitHub repo link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Week5-React
+      <h1>Week5-React</h1>
+      <footer>This project was coded by Catarina S-A and is {""}
+      <a href="https://github.com/casakc/week5-react" target="_blank" rel="noopener noreferrer">open-sourced on GitHub</a></footer>
     </div>
   );
 }
