21ccf2befe27bb1e193787ad3fb50f436f7e3211
casakc <138028480+casakc@users.noreply.github.com>
2024-10-08 12:48:48 +0200

    Included Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Week5-React</h1>
       <footer>This project was coded by Catarina S-A and is {""}
       <a href="https://github.com/casakc/week5-react" target="_blank" rel="noopener noreferrer">open-sourced on GitHub</a></footer>
+    </div>
     </div>
   );
 }
