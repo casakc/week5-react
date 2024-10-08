322e8e6f1981b8f7af836e648b0148753e9b7138
casakc <138028480+casakc@users.noreply.github.com>
2024-10-08 15:30:13 +0200

    Inclued and Edited Weather.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
+import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Week5-React</h1>
+      
+      <Weather/>
       <footer>This project was coded by Catarina S-A and is {""}
       <a href="https://github.com/casakc/week5-react" target="_blank" rel="noopener noreferrer">open-sourced on GitHub</a></footer>
     </div>
