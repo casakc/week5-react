72539e4caeb79e0b7c30ff1b76f50bf76d29e838
casakc <138028480+casakc@users.noreply.github.com>
2024-10-18 23:41:23 +0200

    Edited Temperature Component (C/F)

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -14,22 +14,21 @@ function showCelsius(event) {
     }
 
 function fahrenheit() {
-    return(props.celsius *9)/5 + 32;
+    return((props.celsius *9)/5 + 32);
 }
 
 if (unit==="celsius"){
 return(
     <div className="WeatherTemperature">   
 <span className="temperature">{Math.round(props.celsius)}</span>
-<span className="unit">ºC |{" "}<a href="/" onclick={showFahrenheit}>ºF</a></span>
+<span className="unit">ºC{" "}|{" "}<a href="/" onClick={showFahrenheit}>ºF</a></span>
 </div>
 );
 } else {
-    
-    return (
-        <div className="WeatherTemperature">   
+     return (
+    <div className="WeatherTemperature">   
     <span className="temperature">{Math.round(fahrenheit())}</span>
-    <span className="unit"><a href="/" onclick={showCelsius}>ºC</a> |{" "}ºF</span>
+    <span className="unit"><a href="/" onClick={showCelsius}>ºC</a>{" "}|{" "}ºF</span>
     </div>
     );
 }    
